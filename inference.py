0bbfe39090c815506061312e80ce927630a61271
Jahan Sai Jothimurugan <jahan.jothimurugan@multicorewareinc.com>
2024-07-08 13:41:39 +0530

    files added

---
+++ inference.py
@@ -0,0 +1,40 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+model_path = 'cifar10_cnn_model_augmented.h5'
+model = load_model(model_path)
+
+def preprocess_image(image_path):
+    img = image.load_img(image_path, target_size=(32, 32))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.0 
+    return img_array
+
+def predict_image_class(image_path):
+    img_array = preprocess_image(image_path)
+    predictions = model.predict(img_array)
+    predicted_class = np.argmax(predictions, axis=1)[0]
+    return class_names[predicted_class]
+
+def visualize_prediction(image_path):
+    img = image.load_img(image_path, target_size=(32, 32))
+    predicted_class = predict_image_class(image_path)
+    
+    plt.imshow(img)
+    plt.title(f'Predicted class: {predicted_class}')
+    plt.axis('off')
+    plt.show()
+
+image_path = 'cat.jpg'
+
+predicted_class = predict_image_class(image_path)
+print(f'Predicted class: {predicted_class}')
+
+visualize_prediction(image_path)
